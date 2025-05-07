c17bdbd8ea88702f8930d9cd2b009456e5596cba
Wan Afiq <afiqd503@gmail.com>
2025-05-07 12:39:33 +0800

    update v1

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ app.use(express.json());
 // Routes
 
 // create a todo
-app.post('/todos', async (req, res) => {
+app.post('/todo', async (req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query(
-            'INSERT INTO todos (description) VALUES ($1) RETURNING *',
+            'INSERT INTO todo (description) VALUES ($1) RETURNING *',
             [description]
         );
         res.json(newTodo.rows[0]);
@@ -25,8 +25,27 @@ app.post('/todos', async (req, res) => {
 });
 
 // get all todo
+app.get("/todo", async(req,res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 // get a todo
+app.get("/todo/:id", async(req,res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 // update a todo
 
